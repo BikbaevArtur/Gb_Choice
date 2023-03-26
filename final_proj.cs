990d70bd9cc57157c9e0193bd3fc1afd72c1d1e1
bikbaevartur <bigbyar4i@gmail.com>
2023-03-26 20:27:46 +0300

    Создание функции принта массива

--- final_proj.cs
+++ final_proj.cs
@@ -12,3 +12,13 @@ string [] Array( int size)
     return array;
 }
 
+void ShowArray(string[] array)
+{
+    for(int i = 0; i<array.Length; i++)
+    {
+        Console.Write(array[i] + " , ");
+    }
+}
+
+string[] newarray = Array(3);
+ShowArray(newarray);
